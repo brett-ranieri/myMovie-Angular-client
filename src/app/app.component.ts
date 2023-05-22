41fa8cd154744fb150078464ee7e3672b120b42d
Brett Ranieri <brett.ranieri@gmail.com>
2023-05-22 14:49:24 -0600

    add user registration form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myMovie-Angular-client';
+
+  constructor(public dialog: MatDialog) {}
+
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px',
+    });
+  }
 }
