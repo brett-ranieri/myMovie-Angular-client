05ee18cafd750df0f08d4aa27a9115374c826136
Brett Ranieri <brett.ranieri@gmail.com>
2023-05-23 12:22:40 -0600

    add movie-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-
+import { MovieCardComponent } from './movie-card/movie-card.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -22,6 +22,12 @@ export class AppComponent {
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px',
+    });
+  }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px',
     });
   }
 }
